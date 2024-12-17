301476a63cf68c86abe4c09a7eec0f216e871712
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-17 07:59:36 +0000

    [finish]8章

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_16_101330) do
+ActiveRecord::Schema.define(version: 2024_12_17_073407) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
