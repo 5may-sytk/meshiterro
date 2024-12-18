0957fb8b676e6e9485e4dba0ed1d280e354c64d4
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-18 08:00:40 +0000

    [finish10章

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,23 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImages.new
+  end
+
+  def create
+    @post_image = PostImage.new
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_image_path
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def post_image_params
+    prams.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
