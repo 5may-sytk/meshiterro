36226ff0c808fc308d857cda95c3bfd3226b91dc
5may-sytk <ec2-user@ip-172-31-18-97.eu-north-1.compute.internal>
2024-12-19 11:09:10 +0000

    メンター修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,10 +4,10 @@ class PostImagesController < ApplicationController
   end
 
   def create
-    @post_image = PostImage.new
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_image_path
+    @post_image.save!
+    redirect_to post_image_path(@post_image)
   end
 
   def index
@@ -20,6 +20,6 @@ class PostImagesController < ApplicationController
 
   private
   def post_image_params
-    prams.require(:post_image).permit(:shop_name, :image, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
